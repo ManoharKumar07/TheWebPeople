974f8a7c618c7b11496e3885ac12b3bc9ba0dd05
Manohar Kumar <manohar.csengineer@gmail.com>
2024-12-06 15:10:27 +0530

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/pages/Navbar";
+import Hero from "./components/pages/Hero";
+import Work from "./components/pages/Work";
+import AboutUs from "./components/pages/AboutUs";
+import ContactUs from "./components/pages/ContactUs";
+import Footer from "./components/pages/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>The Web People</div>
+      <div>
+        <Navbar />
+        <Hero />
+        <Work />
+        <AboutUs />
+        <ContactUs />
+        <Footer />
+      </div>
     </>
   );
 }
