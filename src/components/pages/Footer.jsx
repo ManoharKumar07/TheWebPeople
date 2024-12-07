46a93376d04c2beb0ea56d75cf600398c3c9a9e2
Manohar Kumar <manohar.csengineer@gmail.com>
2024-12-07 21:07:53 +0530

    Footer completed

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div className="container px-5">Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content container px-5">
+        <div className="logoandtext">
+          <img
+            src="./Image/Main-Logo-Footer.png"
+            alt="Footer Logo"
+            className="footer-logo"
+          />
+          <p>© 2020 - thewebpeople. All Rights Reserved.</p>
+        </div>
+        <div>
+          <a href="#top" className="back-to-top">
+            ^ BACK TO TOP
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
